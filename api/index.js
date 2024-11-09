7c873b2ab2d366ee3104c86dc632065a2cd8a7f5
Mohamed Haney <123512921+mohamedHaney@users.noreply.github.com>
2024-11-09 06:10:46 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,6 +7,8 @@ import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import cors from 'cors';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
@@ -19,17 +21,13 @@ mongoose
     console.log(err);
   });
 
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
-});
-
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
@@ -54,4 +52,8 @@ app.use((err, req, res, next) => {
     statusCode,
     message,
   });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
