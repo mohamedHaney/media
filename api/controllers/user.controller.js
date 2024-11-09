2bfb48c4ea2f651a462ddf89a84824dd89500d70
mohamedHaney <mohaney745@gmail.com>
2024-11-10 01:51:01 +0200

    #

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -69,7 +69,7 @@ export const deleteUser = async (req, res, next) => {
 export const signout = (req, res, next) => {
   try {
     res
-      .clearCookie('access_token')
+      .clearCookie('acces_token')
       .status(200)
       .json('User has been signed out');
   } catch (error) {
