d7d35f146b6e5ae04ec800f662f98e6fd13e06cf
mohamedHaney <mohaney745@gmail.com>
2024-11-11 00:27:54 +0200

    #

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -45,7 +45,7 @@ export const signin = async (req, res, next) => {
     );
 
     const { password: pass, ...rest } = validUser._doc;
-    res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+    res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest);
   } catch (error) {
     next(error);
   }
@@ -81,7 +81,7 @@ export const google = async (req, res, next) => {
     );
 
     const { password, ...rest } = user._doc;
-    res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+    res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest);
   } catch (error) {
     next(error);
   }
