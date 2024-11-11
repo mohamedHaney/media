4485d500220ad0e3a423ac7cae8eaa824be1b461
mohamedHaney <mohaney745@gmail.com>
2024-11-11 03:35:29 +0200

    #

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -26,7 +26,7 @@ export default function Header() {
 
   const handleSignout = async () => {
     try {
-      const res = await fetch(`https://media-one-rust.vercel.app/api/user/signout`, {
+      const res = await fetch(`/api/user/signout`, {
         method: 'POST',
       });
       const data = await res.json();
