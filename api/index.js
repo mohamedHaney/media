5846234b5bc0be8639c1ec6137d0e0677f61826b
mohamedHaney <mohaney745@gmail.com>
2024-11-13 04:01:41 +0200

    #

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://media-fscj.vercel.app",
   credentials: true,
 }));
 
