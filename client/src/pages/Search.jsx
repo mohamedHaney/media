d42b00cd6049ef3e3f47624508c994c3a3a22151
Mohamed Haney <123512921+mohamedHaney@users.noreply.github.com>
2025-03-26 01:47:10 +0200

    Update Search.jsx

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -124,7 +124,7 @@ export default function Search() {
     <option value='asc'>الأقدم</option>
   </select>
           </div>
-{/*           <div className='flex items-center gap-2'>
+          <div className='flex items-center gap-2'>
             <label className='font-semibold'>التصنيفات:</label>
             <select onChange={handleChange} value={sidebarData.category} id='category'
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
@@ -132,7 +132,7 @@ export default function Search() {
               <option value='الإذاعة و التليفزيون'>الإذاعة و التليفزيون</option>
               <option value='الصحافة'>الصحافة</option>
               </select>
-          </div> */}
+          </div>
           <Button type='submit' outline gradientDuoTone='purpleToPink'>
             ابحث
             </Button>
