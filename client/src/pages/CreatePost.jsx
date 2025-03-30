44254065268e36d991952d45fb1b5151b7c6ff7b
Mohamed Haney <123512921+mohamedHaney@users.noreply.github.com>
2025-03-31 01:13:48 +0200

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -155,7 +155,7 @@ export default function CreatePost() {
             value={formData.content}
             onChange={(value) => setFormData({ ...formData, content: value })}
             placeholder="اكتب شيئًا مميزًا هنا..."
-            className="custom-quill rtl-editor h-[100pxpx]"
+            className="custom-quill rtl-editor h-[100px]"
           />
         </div>
 
