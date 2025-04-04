aa683f013d7f77b2d150498aa972f10012c6fad2
Mohamed Haney <123512921+mohamedHaney@users.noreply.github.com>
2025-04-04 04:50:51 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -29,9 +29,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ["https://documentary-media.vercel.app", "http://localhost:5173"],
+  origin: true, // reflects the request origin
   credentials: true,
 }));
+
 
 
 app.use('/api/user', userRoutes);
